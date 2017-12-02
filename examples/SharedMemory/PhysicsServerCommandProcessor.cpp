e44916b28fa9efd487e0c0c90e4d81af566862cb
dvercaut <dvercaut.vercauteren@ugent.be>
2017-12-02 20:53:59 +0100

    Fixed error in CMD_LOAD_BUNNY

--- examples/SharedMemory/PhysicsServerCommandProcessor.cpp
+++ examples/SharedMemory/PhysicsServerCommandProcessor.cpp
@@ -4947,8 +4947,8 @@ Higher values are generally recommended if faulty rendering and/or incorrect col
 							if (numTris>0)
 							{
 								//btSoftBody*	psb=btSoftBodyHelpers::CreateFromTriMesh(m_data->m_softBodyWorldInfo,gVerticesBunny,&gIndicesBunny[0][0],BUNNY_NUM_TRIANGLES);
-								//btSoftBody*	psb=btSoftBodyHelpers::CreateFromTriMesh(m_data->m_softBodyWorldInfo,&vertices[0],&indices[0],numTris);
-                                btSoftBody* psb=btSoftBodyHelpers::CreatePatch(m_data->m_softBodyWorldInfo, btVector3(-s/2,s+1,0), btVector3(+s/2,s+1,0), btVector3(-s/2,s+1,+s), btVector3(+s/2,s+1,+s), numX,numY, fixed,true);
+								btSoftBody*	psb=btSoftBodyHelpers::CreateFromTriMesh(m_data->m_softBodyWorldInfo,&vertices[0],&indices[0],numTris);
+                //btSoftBody* psb=btSoftBodyHelpers::CreatePatch(m_data->m_softBodyWorldInfo, btVector3(-s/2,s+1,0), btVector3(+s/2,s+1,0), btVector3(-s/2,s+1,+s), btVector3(+s/2,s+1,+s), numX,numY, fixed,true);
                     			btSoftBody::Material*	pm=psb->appendMaterial();
 								pm->m_kLST				=	1.0;
 								pm->m_flags				-=	btSoftBody::fMaterial::DebugDraw;
