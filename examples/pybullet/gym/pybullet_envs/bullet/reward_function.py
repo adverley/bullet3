56657c3387ce3d80638809e3a4801cbd534b47e2
dvercaut <dvercaut.vercauteren@ugent.be>
2018-07-13 15:46:24 +0200

    Implemented advanced sparse reward function

--- examples/pybullet/gym/pybullet_envs/bullet/reward_function.py
+++ examples/pybullet/gym/pybullet_envs/bullet/reward_function.py
@@ -18,6 +18,8 @@ class RewardZoo():
             return cls.torus_distance_reward
         elif(REWARD_TYPE == 'clipped_reward'):
             return cls.clipped_line_distance_reward
+        elif(REWARD_TYPE == 'advanced_sparse_reward'):
+            return cls.advanced_sparse_reward
         else:
             raise Exception('RewardError', 'Reward not found')
 
@@ -251,6 +253,61 @@ class RewardZoo():
                 "Block within the hole. block_pos: {0} : {1}".format(
                     str(block_pos), str(torus_pos)))
             #reward = 10
+
+        env.logger.debug("Reward: %s \n" % str(reward))
+        return reward
+
+    def advanced_sparse_reward(env):
+        """Calculates the reward for the episode.
+        """
+        torus_pos = np.array(
+            p.getBasePositionAndOrientation(env._baxter.torusUid)[0])
+        env.logger.debug("Reward torus position: %s" % str(torus_pos))
+
+        if env._useBlock:
+            block_pos = np.array(
+                p.getBasePositionAndOrientation(env._baxter.blockUid)[0])
+        else:
+            block_pos = np.array(
+                p.getLinkState(env._baxter.baxterUid, 26)[0])  # 26 or avg between 28 and 30
+
+        # Move towards the line through the center of the torus
+        orn = np.array(p.getEulerFromQuaternion(
+            p.getBasePositionAndOrientation(env._baxter.torusUid)[1]))
+
+        # Euler_angle = [yaw, pitch, roll]
+        dir_vector = np.array([math.cos(orn[0]) * math.cos(orn[1]),
+                               math.sin(orn[0]) * math.cos(orn[1]),
+                               math.sin(orn[1])])
+
+        # x0 = p.getJointState(baxterId, 26)[0]
+        x0 = torus_pos
+        x1 = block_pos
+
+        distance = norm((x0 - x1) - vdot((x0 - x1), dir_vector) * dir_vector)
+        reward = -10
+
+        tan_a = env._baxter.torusRad / torus_pos[0]
+        env.logger.debug("Distance:", distance, "Cone height:", tan_a * x1[0])
+
+        # Only valid if torus angle doesn't change over iterations
+        if (distance < tan_a * x1[0]):
+            # distance = norm(torus_pos - block_pos)
+            reward = 100
+
+        x_bool = (torus_pos[0] + env._baxter.margin) < block_pos[0]
+        y_bool = (torus_pos[1] - env._baxter.torusRad) < block_pos[1] and (torus_pos[1] + env._baxter.torusRad) > \
+                 block_pos[1]
+        z_bool = (torus_pos[2] - env._baxter.torusRad) < block_pos[2] and (torus_pos[2] + env._baxter.torusRad) > \
+                 block_pos[2]
+
+        if y_bool and z_bool and x_bool:
+            env.logger.debug(
+                "Block within the hole. block_pos: {0} torus_pos: {1}".format(
+                    str(block_pos), str(torus_pos)))
+            env.logger.debug("env._envStepCounter: %s" %
+                             str(env._envStepCounter))
+            reward = 10000
 
         env.logger.debug("Reward: %s \n" % str(reward))
         return reward
