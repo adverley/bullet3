97d096ac1d46f757e7c947ba19b7e73482b427c7
dvercaut <dvercaut.vercauteren@ugent.be>
2018-08-01 17:30:43 +0200

    Added zone paritioned memory to supervised RL example

--- examples/pybullet/gym/pybullet_envs/baselines/baxterSetup/supervised_rl.py
+++ examples/pybullet/gym/pybullet_envs/baselines/baxterSetup/supervised_rl.py
@@ -13,7 +13,7 @@ from keras.optimizers import Adam, RMSprop
 from tqdm import tqdm
 
 class ModelAgent:
-    def __init__(self, data, lr, n1, n2, bs, target_net_update_freq):
+    def __init__(self, data, lr, n1, n2, bs, target_net_update_freq, zone_queue=False):
         self.lr = lr
         self.state_space = 10
         self.action_space = 4
@@ -21,9 +21,34 @@ class ModelAgent:
         self.target_model = self.create_model(n1, n2)
 
         self.target_net_update_freq = target_net_update_freq
+        self.zone_queue = zone_queue
+
+        if zone_queue:
+            #self.zone_rewards = {'outside': -10, 'cone': 100, 'torus_col': -10000, 'completion': 10000}
+            self.zone_rewards = {'outside': -0.1, 'cone': 1, 'torus_col': -1, 'completion': 10}
+            self.zone_dist = [0.25, 0.25, 0.25, 0.25]
+            self.zone_idx = [0, 0, 0, 0]
+
+            self.memory_outside = []
+            self.memory_cone = []
+            self.memory_coll = []
+            self.memory_completion = []
+            for i in data:
+                if int(i[2]) == self.zone_rewards['outside']:
+                    self.memory_outside.append(i)
+                elif int(i[2]) == self.zone_rewards['cone']:
+                    self.memory_cone.append(i)
+                elif int(i[2]) == self.zone_rewards['torus_col']:
+                    self.memory_coll.append(i)
+                elif int(i[2]) == self.zone_rewards['completion']:
+                    self.memory_completion.append(i)
+                else:
+                    print("Unrecognized sample!")
+                    exit(-1)
+        else:
+            self.memory = random.sample(data, len(data))  # deque(maxlen=self.mem_init_size)
         # Create train test set split, memory is train set loss calculated on test set.
 
-        self.memory = random.sample(data, len(data))  #deque(maxlen=self.mem_init_size)
         self.batch_idx = 0
         self.batch_size = bs
         self.tau = .125
@@ -56,10 +81,46 @@ class ModelAgent:
 
         #Since data is shuffled, batch could be subsequent samples
         #sample = random.sample(self.memory, self.batch_size)
+        def create_sample(memory, bs, use_idx=None):
+            sample = []
+            if use_idx is None:
+                idx = self.batch_size
+            else:
+                idx = self.zone_idx[use_idx]
+
+            for i in range(bs):
+                sample.append(memory[idx % len(memory)])
+                if use_idx is None:
+                    self.batch_idx += 1
+                    idx = self.batch_size
+                else:
+                    self.zone_idx[use_idx] += 1
+                    idx = self.zone_idx[use_idx]
+            return sample
+
         sample = []
-        for i in range(self.batch_size):
-            sample.append(self.memory[self.batch_idx % len(self.memory)])
-            self.batch_idx += 1
+
+        if self.zone_queue:
+            bs = self.batch_size
+            sample1 = create_sample(self.memory_outside, int(self.batch_size*self.zone_dist[0]), 0)
+                #random.sample(self.memory_outside, int(self.batch_size*self.zone_dist[0]))
+            bs -= int(self.batch_size*self.zone_dist[0])
+            sample2 = create_sample(self.memory_cone, int(self.batch_size*self.zone_dist[1]), 1)
+                #random.sample(self.memory_cone, int(self.batch_size*self.zone_dist[1]))
+            bs -= int(self.batch_size * self.zone_dist[1])
+            sample3 = create_sample(self.memory_coll, int(self.batch_size*self.zone_dist[2]), 2)
+                #random.sample(self.memory_coll, int(self.batch_size*self.zone_dist[2]))
+            bs -= int(self.batch_size * self.zone_dist[2])
+            sample4 = create_sample(self.memory_completion, bs, 3)
+                #random.sample(self.memory_completion, bs)
+
+            #print("\nBatch composition:", len(sample1), len(sample2), len(sample3), len(sample4))
+            sample = sample1 + sample2 + sample3 + sample4
+        else:
+            sample = create_sample(self.memory, self.batch_size)
+            #for i in range(self.batch_size):
+            #    sample.append(self.memory[self.batch_idx % len(self.memory)])
+            #    self.batch_idx += 1
 
         for s in sample:
             #state, action, reward, new_state, done = sample
@@ -100,23 +161,24 @@ class ModelAgent:
         print("\n{}/{}".format(ep, ep_tot),
               "Execution time: {} steps/s".format(round(steps/time, 2)),
               "Mae:", np.average(self.mae),
-              "Loss:", np.average(self.loss)
+              "Loss:", np.average(self.loss),
+              "\n"
              ) #trail_len
 
 def main(args):
     configs = [
                 #Test impact update_freq
-                (0.0001, 32, 16, 32, 10000),
+                #(0.0001, 32, 16, 32, 10000),
                 (0.0001, 32, 16, 64, 10000),
-                (0.0001, 32, 16, 128, 10000),
-                (0.0001, 64, 32, 32, 10000),
+                #(0.0001, 32, 16, 128, 10000),
+                #(0.0001, 64, 32, 32, 10000),
                 (0.0001, 64, 32, 64, 10000),
-                (0.0001, 64, 32, 128, 10000),
+                #(0.0001, 64, 32, 128, 10000),
 
                 #Test impact larger network
-                (0.0001, 1024, 512, 32, 10000),
+                #(0.0001, 1024, 512, 32, 10000),
                 (0.0001, 1024, 512, 64, 10000),
-                (0.0001, 1024, 512, 128, 10000),
+                #(0.0001, 1024, 512, 128, 10000),
 
                ] #(lr, n1, n2, bs, gamma)
 
@@ -136,7 +198,7 @@ def main(args):
         data = json.load(fp)
 
     for exp in tqdm(configs):
-        agent = ModelAgent(data, *exp)
+        agent = ModelAgent(data, *exp, zone_queue=args.zone_queue)
         tot_step = 0
 
         for ep in tqdm(range(EPISODES)):
@@ -144,11 +206,6 @@ def main(args):
             for step in range(trial_len):
                 agent.train_batch() #Internally iterates default (prediction) model
                 tot_step += 1  # Used to determine replay memory update
-
-                if tot_step % agent.target_net_update_freq == 0:
-                    #print("Updating target network...")
-                    #agent.target_train()  # iterates target model
-                    pass
 
             if ep % 2000 == 0:
                 agent.print_stats(ep, EPISODES, time.time() - start_time, step)
@@ -182,5 +239,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', type=str, default='baxter_dqn_exp26_config_net2layer_small_mem_log.json')
     parser.add_argument('-out', type=str, default='test')
+    parser.add_argument('-zone_queue', type=bool, default=False)
     args = parser.parse_args()
     main(args)
